7d26790ea7ba422630180bae8fabfddf64a92de3
Christopher D Koon <CHRKOON@uat.edu>
2018-09-13 10:17:28 -0700

    Started Project
    Added GameManager and Controllers. Started TankData. WIP.

---
+++ Assets/Scripts/Controllers/Controller_Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller_Player : MonoBehaviour
+{
+
+
+	void Start ()
+    {
+
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
