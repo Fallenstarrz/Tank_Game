bc2e51677a2f549bd3a2640a85e501d2a2000541
Christopher D Koon <CHRKOON@uat.edu>
2018-11-20 19:19:21 -0700

    Started Milestone 4

--- Assets/Scripts/UI/UI_Health.cs
+++ Assets/Scripts/UI/UI_Health.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UI_Health : MonoBehaviour {
+public class UI_Health : MonoBehaviour
+{
+    public TankData data;
+    public Slider healthBar;
 
 	// Use this for initialization
 	void Start ()
-    {
-		
+	{
+	    data = GetComponent<TankData>();
+	    healthBar = GetComponent<Slider>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
-    {
-		
-	}
+	{
+        healthBar.value = data.healthCurrent / data.healthMax;
+    }
 }
