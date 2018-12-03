b82e3ba49dc20e611f1e7635f11f0fae05a8f62c
Christopher Koon <chrkoon@uat.edu>
2018-12-03 12:43:00 -0700

    Added Controller Support

--- Assets/Scripts/Controllers/Controller_Player.cs
+++ Assets/Scripts/Controllers/Controller_Player.cs
@@ -7,6 +7,14 @@ public class Controller_Player : MonoBehaviour
     [HideInInspector] private TankData data;
     [HideInInspector] private TankMotor motor;
 
+    public enum controlType
+    {
+        wasd,
+        arrows,
+        controller
+    }
+    public controlType selectedController;
+
     void Start()
     {
         data = GetComponent<TankData>();
@@ -17,6 +25,22 @@ public class Controller_Player : MonoBehaviour
 
 
 	void Update ()
+    {
+        switch (selectedController)
+        {
+            case controlType.wasd:
+                wasdControls();
+                break;
+            case controlType.arrows:
+                arrowControls();
+                break;
+            case controlType.controller:
+                controllerControls();
+                break;
+        }
+    }
+
+    private void wasdControls()
     {
         if (Input.GetButton("Shoot1"))
         {
@@ -52,6 +76,64 @@ public class Controller_Player : MonoBehaviour
             Vector3 vectorRotation = Vector3.up * data.rotationSpeed * Time.deltaTime;
             motor.rotate(-vectorRotation);
         }
+    }
+
+    private void arrowControls()
+    {
+        if (Input.GetButton("Shoot1P2"))
+        {
+            // Shoot machine gun
+            motor.ShootBullet();
+        }
+        if (Input.GetButton("Shoot2P2"))
+        {
+            // Shoot cannon
+            motor.ShootMissile();
+        }
+        if (Input.GetButton("MoveForwardP2"))
+        {
+            // Move Forward
+            Vector3 movementVector = (Vector3.forward * data.movementSpeed * Time.deltaTime);
+            motor.move(movementVector);
+        }
+        if (Input.GetButton("MoveBackwardP2"))
+        {
+            // Move Backward
+            Vector3 movementVector = (Vector3.forward * data.movementSpeed * Time.deltaTime);
+            motor.move(-movementVector);
+        }
+        if (Input.GetButton("RotateRightP2"))
+        {
+            // Move Right
+            Vector3 vectorRotation = Vector3.up * data.rotationSpeed * Time.deltaTime;
+            motor.rotate(vectorRotation);
+        }
+        if (Input.GetButton("RotateLeftP2"))
+        {
+            // Move Left
+            Vector3 vectorRotation = Vector3.up * data.rotationSpeed * Time.deltaTime;
+            motor.rotate(-vectorRotation);
+        }
+    }
+
+    private void controllerControls()
+    {
+        if (Input.GetButton("CShoot1"))
+        {
+            // Shoot machine gun
+            motor.ShootBullet();
+        }
+        if (Input.GetButton("CShoot2"))
+        {
+            // Shoot cannon
+            motor.ShootMissile();
+        }
+        // Move Forward
+        Vector3 movementVector = (-Vector3.forward * data.movementSpeed * Time.deltaTime * Input.GetAxis("CMoveForward"));
+        motor.move(movementVector);
+        // Move Right
+        Vector3 vectorRotation = (Vector3.up * data.rotationSpeed * Time.deltaTime * Input.GetAxis("CRotateRight"));
+        motor.rotate(vectorRotation);
     }
 
     private void OnDestroy()
